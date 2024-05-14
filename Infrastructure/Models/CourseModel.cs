71a744e6cd1ef8eab0c46afaa075c0c77416b408
Bjorn-23 <bjornandersson84@gmail.com>
2024-05-14 09:18:43 +0200

    Added logic for togglng mobile menu replacing js. Also changed so that courses go to api in azure. Fixed subscribe on home page to work in blazor, need to attach servicebus to it so we can create subscriptions.

--- Infrastructure/Models/CourseModel.cs
+++ Infrastructure/Models/CourseModel.cs
@@ -39,16 +39,15 @@ public class CourseModel
 
     public int NumberOfLikes { get; set; } = 0;
 
-    //public string CourseDescription { get; set; } = null!;
-
     public int? CategoryId { get; set; }
 
-    public string? Category { get; set; }
+    public CategoryModel Category { get; set; } = new();
 
-    //public int ContentId { get; set; }
-    //public CourseContentModel Content { get; set; } = new();
-       
-    //public virtual ICollection<ProgramDetailsModel> ProgramDetails { get; set; } = new List<ProgramDetailsModel>();
-    //public virtual ICollection<CourseIncludesModel> Includes { get; set; } = new List<CourseIncludesModel>();
+    public string CourseDescription { get; set; } = null!;
 
+    public int CourseContentId { get; set; }
+    public CourseContentModel CourseContent { get; set; } = new();
+
+    public virtual ICollection<ProgramDetailsModel> ProgramDetails { get; set; } = new List<ProgramDetailsModel>();
+    public virtual ICollection<CourseIncludesModel> CourseIncludes { get; set; } = new List<CourseIncludesModel>();
 }
