6e80fd34bf639d044bfe36de14a8ccad2b2875ff
Bjorn-23 <bjornandersson84@gmail.com>
2024-05-20 23:55:40 +0200

    refactored CategoryModel to have value and name. This way I can display one text and use the other for queries...

--- Infrastructure/Services/CoursesService.cs
+++ Infrastructure/Services/CoursesService.cs
@@ -76,7 +76,7 @@ public class CoursesService
 
             var queryJson = JsonConvert.SerializeObject(query);
             var content = new StringContent(queryJson, Encoding.UTF8, "application/json");
-            var allCoursesResponse = await _httpClient.PostAsync($"{_configuration["ConnectionStrings:GraphQlApi"]}", content);
+            var allCoursesResponse = await _httpClient.PostAsync($"{_configuration["ConnectionStrings:LocalGraphQlApi"]}", content);
             if (allCoursesResponse.IsSuccessStatusCode)
             {
                 var json = await allCoursesResponse.Content.ReadAsStringAsync();
